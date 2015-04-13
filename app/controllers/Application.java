cb88d163316425490a08c21220a0c18aff7c6422
Noel Kawano <noelmk@hawaii.edu>
2015-04-13 05:46:33 -1000

    It works

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,37 @@
 package controllers;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.formdata.ContactFormData;
 import views.html.Index;
-import views.html.Page1;
+import views.html.newContact;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Provides controllers for this application.
  */
 public class Application extends Controller {
 
+
+  private static void writeToFile(String textToWrite) {
+    final String outputPath = "C:\\Users\\NOEL\\Desktop\\Data.txt";
+
+    try {
+      FileWriter fileWriter = new FileWriter(outputPath);
+      fileWriter.write(textToWrite);
+      fileWriter.close();
+      System.out.println("Success!");
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
   /**
    * Returns the home page.
+   *
    * @return The resulting home page.
    */
   public static Result index() {
@@ -19,12 +39,27 @@ public class Application extends Controller {
   }
 
   /**
-   * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The Page1.
+   * Returns newContact, a simple example of a second page to illustrate navigation.
+   *
+   * @return The newContact.
    */
-  public static Result page1() {
-    return ok(Page1.render("Welcome to Page1."));
+  public static Result newContact() {
+    Form<ContactFormData> formData = Form.form(ContactFormData.class);
+    return ok(newContact.render(formData));
 
   }
+
+  /**
+   * Returns a postContact.
+   * @return the postContact.
+   */
+  public static Result postContact() {
+    Form<ContactFormData> formData = Form.form(ContactFormData.class).bindFromRequest();
+    ContactFormData data = formData.get();
+    System.out.printf("Got data: %s %s %s %s %n", data.eq, data.wave, data.sen, data.trail);
+    writeToFile(data.eq + "\n" + data.wave + "\n" + data.sen + "\n" + data.trail);
+    return ok(newContact.render(formData));
+  }
+
 
 }
---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,15 @@
+package views.formdata;
+
+/**
+ * The backing class for the Contact form.
+ */
+public class ContactFormData {
+  /** The first name. */
+  public String eq;
+  /** The last name. */
+  public String wave;
+  /** The telephone number in xxx-xxx-xxxx format. */
+  public String sen;
+  public String trail;
+
+}
